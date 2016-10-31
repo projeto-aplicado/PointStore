fbe73283f86b8fddc13ee7353697029d052b2ecc
romulo-xavier25 <romulo.xavier25@gmail.com>
2016-10-31 10:49:53 -0300

    arquitetura spa com angularjs e javaEE

--- front-end/app/js/app.js
+++ front-end/app/js/app.js
@@ -2,11 +2,11 @@ var app = angular.module('sistemaVendaDePontosDeVantagem', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view/sistema/home', {
-        templateUrl: 'pagina_home.html',
+        templateUrl: 'view/sistema/pagina_home.html',
         controller: 'controller/sistema/homeController'
     });
     $routeProvider.when('/perfil', {
-        templateUrl: 'perfil.html',
+        templateUrl: 'view/sistema/perfil.html',
         controller: 'controller/sistema/perfilController'
     });
     $routeProvider.when('/meu_perfil', {
@@ -14,23 +14,23 @@ app.config(['$routeProvider', function($routeProvider) {
         controller: 'controller/sistema/perfilController'
     });
     $routeProvider.when('/meus_pontos', {
-        templateUrl: 'meus_pontos.html',
+        templateUrl: 'view/sistema/meus_pontos.html',
         controller: 'controller/sistema/perfilController'
     });
     $routeProvider.when('/cadastrar_vendas', {
-        templateUrl: 'cadastrar_vendas.html',
+        templateUrl: 'view/sistema/cadastrar_vendas.html',
         controller: 'controller/usuario/vendasController'
     });
     $routeProvider.when('/listar_vendas', {
-        templateUrl: 'listar_vendas.html',
+        templateUrl: 'view/sistema/listar_vendas.html',
         controller: 'controller/usuario/vendasController'
     });
     $routeProvider.when('/minhas_compras', {
-        templateUrl: 'minhas_compras.html',
+        templateUrl: 'view/sistema/minhas_compras.html',
         controller: 'controller/usuario/comprasController'
     });
     $routeProvider.when('/loja', {
-        templateUrl: 'loja.html',
+        templateUrl: 'view/sistema/loja.html',
         controller: 'controller/usuario/lojaController'
     });
     $routeProvider.otherwise({redirectTo: '/view/sistema/home'});
