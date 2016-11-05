a4920c542914260c0ecdcd545af663a13900449a
raquelmeireles <raquelpier.26@gmail.com>
2016-11-05 17:01:07 -0300

    atualizando a parte de login

--- front-end/app/js/controller/login/loginController.js
+++ front-end/app/js/controller/login/loginController.js
@@ -1,5 +1,14 @@
-app.controller('loginController', function($scope) {
+var app = angular.module('loginController', ['ngResource'], ['ngRoute']);
 
-	//
+app.controller('loginController', function sistemaLogin($scope) {
+
+	$scope.login = null;
+	$scope.senha = null;
+
+	$scope.logar = function login(login, senha) {
+			this.login = login;
+			this.senha = senha;
+	}
+
 
 });
